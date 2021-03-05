d0285108091168f05940726531e8fd8dff99e527
ttumasaki <be_da_masa@ybb.ne.jp>
2021-03-05 23:08:29 +0900

    DBとの接続

--- database/migrations/2021_03_05_230503_create-memory.php
+++ database/migrations/2021_03_05_230503_create-memory.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMemoriesTable extends Migration
+class CreateMemory extends Migration
 {
     /**
      * Run the migrations.
@@ -15,6 +15,15 @@ class CreateMemoriesTable extends Migration
     {
         Schema::create('memories', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('point',30);
+            $table->integer('size');
+            $table->integer('w_condition');
+            $table->integer('number');
+            $table->integer('state');
+            $table->integer('direction');
+            $table->integer('people');
+            $table->float('temperature',2,1);
+            $table->binary('image');
             $table->timestamps();
         });
     }
@@ -26,6 +35,6 @@ class CreateMemoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('memories');
+        //
     }
 }
